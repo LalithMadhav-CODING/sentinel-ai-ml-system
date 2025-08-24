a41c980dc28b7157c76785446ca2f4ba13a67a52
LalithMadhav-CODING <lalithmadhavcoding@gmail.com>
2025-08-24 22:12:54 +0530

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="relative w-full h-screen">
       <MatrixBackground />
       <div className="absolute inset-0 z-10 flex items-center justify-center">
-       <ChatWindow />
+        <ChatWindow />
       </div>
     </div>
   );
